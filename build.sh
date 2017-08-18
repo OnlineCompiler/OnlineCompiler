a948f094df3ae1a20f7d9939fc3bf54cf26a0c80
iflei <admin@iflei.com>
2017-08-19 00:20:18 +0800

    update sh conf

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-rm -rf log bin; mkdir log bin
-cd src; make clean; make release
+rm -rf log bin tmp wwwroot/cgi-bin; mkdir log bin tmp wwwroot/cgi-bin
+cd src; make clean; make release;cd -
+cd cgi; make clean; make; mv compiler ../wwwroot/cgi-bin
